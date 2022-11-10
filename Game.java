810391f3fa065bede859676714a84db15b44f743
Hasnain Ali <hasnainali.2005@hotmail.com>
2022-11-10 18:07:09 -0500

    final

--- Game.java
+++ Game.java
@@ -28,6 +28,8 @@ class Game {
         Player playerOne = new Player("", 300, 20);
         Player playerTwo = new Player("", 600, 10);
 
+        String winner = "";
+
 
         Scanner input = new Scanner(System.in);
 
@@ -36,12 +38,12 @@ class Game {
         System.out.println("Enter your name (Player two)");
         playerTwo.setName(input.nextLine());
 
-        while ((playerOne.getHp() >= 10) && (playerTwo.getHp() >= 0)) {
+        while (true) {
             System.out.println(playerOne.getName() + " hp: " + playerOne.getHp());
             System.out.println(playerTwo.getName() + " hp: " + playerTwo.getHp());
             while (true) {
 
-                System.out.println("Player 1 Options: 10 for x10 dmg, 3 for x3 dmg, 0 for x0 dmg, 10 for x10dmg, p for pass");
+                System.out.println("Player 1 Options: 10 for x10 dmg, 3 for x3 dmg, 2 for x2 dmg, 0 for x0 dmg, p for pass");
                 String option = input.nextLine();
 
                 if (option.equals("2")) {
@@ -58,12 +60,19 @@ class Game {
                 } else if (option.equals("10")) {
                     playerTwo.setHp(playerTwo.getHp() - tenTimeDmg(playerOne.getDmg()));
                     break;
-                } 
+                } else {
+                    System.out.println("Enter a valid option!");
+                }
             }
+
+            if (playerTwo.hp <= 0) {
+                winner = "Wizard";
+                break;
+            } 
 
             while (true) {
 
-                System.out.println("Player 2 Options: 2 for x2 dmg, 3 for x3 dmg, 0 for x0 dmg, 10 for x10dmg, p for pass");
+                System.out.println("Player 2 Options: 10 for x10 dmg, 3 for x3 dmg, 2 for x2 dmg, 0 for x0 dmg, p for pass");
                 String option = input.nextLine();
 
                 if (option.equals("2")) {
@@ -78,22 +87,29 @@ class Game {
                 } else if (option.equals("p")) {
                     break;
                 } else if (option.equals("10")) {
-                    playerOne.setHp(playerTwo.getHp() - tenTimeDmg(playerOne.getDmg()));
+                    playerOne.setHp(playerOne.getHp() - tenTimeDmg(playerTwo.getDmg()));
                     break;
+                } else {
+                    System.out.println("Enter a valid option!");
                 }
 
-
             }
+
+            if (playerOne.hp <= 0) {
+                winner = "Dragon";
+                break;
+            } 
+
 
         }
 
         System.out.println(playerOne.getName() + " hp: " + playerOne.getHp());
         System.out.println(playerTwo.getName() + " hp: " + playerTwo.getHp());
+        System.out.println("\n" + winner + " wins!");
     
         input.close();
         
     }
 }
-
 
 
